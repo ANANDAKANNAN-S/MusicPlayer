b49a2deccb1e1d02a16c4e69bb212033a2ef9c6e
ANANDAKANNAN-S <147751967+ANANDAKANNAN-S@users.noreply.github.com>
2023-11-14 23:49:55 +0530

    Add files via upload

---
+++ sync3/data.js
@@ -0,0 +1,30 @@
+let songs=[
+    {
+        name:"I'm Scared",
+        path:'iamscared.mpeg',
+        artist:'Anirudh Ravichander',
+        cover:'scared.jpg'
+        ,
+    },
+    {
+        name:"Ordinary Person",
+        path:'op.mpeg',
+        artist:'Anirudh Ravichander',
+        cover:'op.jpeg'
+        ,
+    },
+    {
+        name:"Master The Blaster",
+        path:'mb.mpeg',
+        artist:'Anirudh Ravichander',
+        cover:'mb.jpg'
+        ,
+    },
+    {
+        name:"Wasted",
+        path:'w.mpeg',
+        artist:'Anirudh Ravichander',
+        cover:'w.jpg'
+        ,
+    }
+]
